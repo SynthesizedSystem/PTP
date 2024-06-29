8d48b731190c1de7b5fce92a610e1b3827c2c107
Max Ivanov <maks.ivanov.developer@gmail.com>
2024-06-29 21:11:01 +0200

    Add JavaDoc comments

--- Aufgabe6/src/main/java/org/aufgabe6/Brettspiel.java
+++ Aufgabe6/src/main/java/org/aufgabe6/Brettspiel.java
@@ -3,11 +3,21 @@ package org.aufgabe6;
 import java.util.Arrays;
 import java.util.Objects;
 
+/**
+ * Represents a board game with a title, a range of players, and a minimum age requirement.
+ */
 public class Brettspiel {
     private final String titel;
     private final int[] spielerAnzahl = new int[2];
     private final int mindestAlter;
 
+    /**
+     * Constructs a new Brettspiel instance.
+     *
+     * @param titel         the title of the board game
+     * @param spielerAnzahl an array with two elements, representing the minimum and maximum number of players
+     * @param mindestAlter  the minimum age required to play the game
+     */
     Brettspiel(String titel, int[] spielerAnzahl, int mindestAlter) {
         this.titel = titel;
         this.spielerAnzahl[0] = spielerAnzahl[0];
@@ -15,23 +25,49 @@ public class Brettspiel {
         this.mindestAlter = mindestAlter;
     }
 
+    /**
+     * Gets the minimum age required to play the game.
+     *
+     * @return the minimum age
+     */
     public int getMindestAlter() {
         return mindestAlter;
     }
 
+    /**
+     * Gets the range of players for the game.
+     *
+     * @return an array with two elements: the minimum and maximum number of players
+     */
     public int[] getSpielerAnzahl() {
         return spielerAnzahl;
     }
 
+    /**
+     * Gets the title of the game.
+     *
+     * @return the title of the game
+     */
     public String getTitel() {
         return titel;
     }
 
+    /**
+     * Returns a string representation of the game, including its title, player range, and minimum age.
+     *
+     * @return a string representation of the game
+     */
     @Override
     public String toString() {
         return String.format("%s (%d-%d Spieler:innen, ab %d Jahren)", titel, spielerAnzahl[0], spielerAnzahl[1], mindestAlter);
     }
 
+    /**
+     * Compares this game to the specified object for equality.
+     *
+     * @param o the object to compare with
+     * @return true if the specified object is equal to this game, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -42,13 +78,20 @@ public class Brettspiel {
                 && Objects.deepEquals(getSpielerAnzahl(), that.getSpielerAnzahl());
     }
 
+    /**
+     * Returns a hash code value for the game.
+     *
+     * @return a hash code value for the game
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getTitel(), Arrays.hashCode(getSpielerAnzahl()), getMindestAlter());
     }
 
+    /**
+     * Prints the string representation of the game to the standard output.
+     */
     public void ausgeben() {
         System.out.println(this.toString());
     }
-
 }
--- Aufgabe6/src/main/java/org/aufgabe6/BrettspielSammlung.java
+++ Aufgabe6/src/main/java/org/aufgabe6/BrettspielSammlung.java
@@ -2,10 +2,19 @@ package org.aufgabe6;
 
 import java.util.Arrays;
 
+/**
+ * Represents a collection of board games.
+ */
 public class BrettspielSammlung {
     private Brettspiel[] brettspielen = new Brettspiel[1];
     private int anzahlBrettspiele = 0;
 
+    /**
+     * Returns the index of the specified board game in the collection.
+     *
+     * @param bs the board game to find
+     * @return the index of the board game, or -1 if it is not found
+     */
     public int getIndexFuer(Brettspiel bs) {
         for (int i = 0; i < brettspielen.length; i++) {
             if (bs.equals(brettspielen[i])) {
@@ -15,14 +24,32 @@ public class BrettspielSammlung {
         return -1;
     }
 
+    /**
+     * Returns the number of board games in the collection.
+     *
+     * @return the number of board games
+     */
     public int getAnzahlBrettspiele() {
         return anzahlBrettspiele;
     }
 
+    /**
+     * Returns the board game at the specified index.
+     *
+     * @param index the index of the board game
+     * @return the board game at the specified index
+     * @throws ArrayIndexOutOfBoundsException if the index is out of range
+     */
     public Brettspiel getBrettspielAnIndex(int index) {
         return brettspielen[index];
     }
 
+    /**
+     * Adds a new board game to the collection.
+     *
+     * @param bs the board game to add
+     * @throws IllegalArgumentException if the board game is null
+     */
     void hinzufuegen(Brettspiel bs) {
         if (bs == null) {
             throw new IllegalArgumentException();
@@ -33,6 +60,11 @@ public class BrettspielSammlung {
         brettspielen[anzahlBrettspiele++] = bs;
     }
 
+    /**
+     * Removes a board game from the collection.
+     *
+     * @param bs the board game to remove
+     */
     void entfernen(Brettspiel bs) {
         int index = getIndexFuer(bs);
         if (index == -1) {
@@ -44,18 +76,26 @@ public class BrettspielSammlung {
         brettspielen[--anzahlBrettspiele] = null;
     }
 
+    /**
+     * Returns a string representation of the collection, including the number of board games and their details.
+     *
+     * @return a string representation of the collection
+     */
     @Override
     public String toString() {
         StringBuilder ret = new StringBuilder();
-        ret.append(String.format("%d Brettspielen\n", anzahlBrettspiele));
+        ret.append(String.format("%d Brettspiele\n", anzahlBrettspiele));
         for (Brettspiel bs : brettspielen) {
             if (bs != null) {
                 ret.append(bs.toString()).append('\n');
             }
-        };
+        }
         return ret.toString();
     }
 
+    /**
+     * Clears the collection of all board games.
+     */
     void leeren() {
         brettspielen = new Brettspiel[brettspielen.length];
         anzahlBrettspiele = 0;
